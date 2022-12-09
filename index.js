00c466c35f59d06de89ee09e32b63195937630bc
Stellamargy <stella.oloo@student.moringaschool.com>
2022-12-09 19:00:26 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+ // Write your algorithm here
+ const str =("string")
+ const isPalindrome =(newStr) => {
+   newStr= newStr.toLowerCase()
+   for (let i = 0; i < newStr.length / 2; i++){
+     if (newStr[i] !== newStr[newStr.length - 1 - i]){
+       return false;
+     }
+   }
+   return true
+ }
+
+console.log(isPalindrome(str))
+
 
 /* 
-  Add your pseudocode here
+ Add your pseudocode here
+This should be in a way such, whenever the word is read backwards, it hs the same wording as from the start.
+For example, civic read forward and backwards sounds similar
+
 */
 
 /*
-  Add written explanation of your solution here
+ Add written explanation of your solution here
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+ // add your own custom tests in here
+ console.log('Expecting: true');
+ console.log('=>', isPalindrome('civic'))
 
-  console.log("");
+console.log(' ')
+ 
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+ console.log("Expecting: true");
+ console.log("=>", isPalindrome("racecar"));
+
+ console.log("");
+
+ console.log("Expecting: false");
+ console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
